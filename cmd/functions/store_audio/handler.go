ae7c78f993373748c9afe55128e79b0c844ca618
Lucas Andrade Flores <lcs.oliveira93@gmail.com>
2024-01-22 21:06:04 +0100

    Implement store_audio handler

--- cmd/functions/store_audio/handler.go
+++ cmd/functions/store_audio/handler.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"context"
+	"log"
+
+	"github.com/LucasAndFlores/go_lambdas_project/config"
+	"github.com/LucasAndFlores/go_lambdas_project/internal/service"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Response struct {
-	StatusCode int         `json:"statusCode"`
-	Body       interface{} `json:"body"`
+type HttpRequest = events.APIGatewayProxyRequest
+
+type HttpBodyResponse = map[string]interface{}
+
+type HttpResponse struct {
+	StatusCode int              `json:"statusCode"`
+	Body       HttpBodyResponse `json:"body"`
 }
 
-func handler(request events.APIGatewayProxyRequest) (Response, error) {
-	return Response{
-		StatusCode: 200,
-		Body:       "ok",
+type handler struct {
+	service service.IAudioService
+}
+
+func (h *handler) handleRequest(ctx context.Context, request HttpRequest) (HttpResponse, error) {
+	statusCode, body := h.service.GeneratePreSignedPutURL(request.Body, ctx)
+
+	return HttpResponse{
+		StatusCode: statusCode,
+		Body:       body,
 	}, nil
 }
 
 func main() {
-	lambda.Start(handler)
+	preSigned, err := config.LoadPreSignedClient(context.Background())
+
+	if err != nil {
+		log.Fatalf("An error occurred when tried to load AWS config. Error: %v", err)
+	}
+
+	s := service.NewAudioService(preSigned)
+	h := handler{service: s}
+
+	lambda.Start(h.handleRequest)
 }
 
